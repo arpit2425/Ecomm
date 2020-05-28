183e77c2ff06cc9093c9785890f3ca4eec9c2550
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-28 22:52:40 +0530

    bug fixed

--- src/components/Products/FeaturedProducts.js
+++ src/components/Products/FeaturedProducts.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ProductList from "./ProductList";
-import { ProductContext } from "../../context/products";
+// import { ProductContext } from "../../context/products";
 import Loading from "../Loading";
 
 export default function FeaturedProducts() {
-  const { loading, featured } = React.useContext(ProductContext);
-  if (loading) {
-    return <Loading />;
-  }
-  return <ProductList title="featured products" products={featured} />;
+  //   const { loading, featured } = React.useContext(ProductContext);
+  // if (loading) {
+  //   return <Loading />;
+  //   // }
+  //   return <ProductList title="featured products" products={featured} />;
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
           our products
         </Link>
       </Hero>
-      <FeaturedProducts />
+      {/* <FeaturedProducts /> */}
     </>
   );
 }
