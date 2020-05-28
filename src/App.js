a2ca0bb4ba46ab12d97ad39b7ba0a9b74655899f
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-28 16:18:02 +0530

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Error from "./pages/Error";
 export default function App() {
-  return <h1>hello from app component</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
