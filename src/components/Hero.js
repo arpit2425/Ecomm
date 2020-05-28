b1a417551d43df97fbdd85d14980916e512a3349
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-28 22:45:22 +0530

    tags

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,8 @@ export default function Hero({ children }) {
   return (
     <div className="hero">
       <div className="banner">
-        <h1>think, code, deploy</h1>
-        <p>embrace your choices - we do</p>
+        <h1>Under Construction</h1>
+        <p>This site is under construction</p>
         {children}
       </div>
     </div>
