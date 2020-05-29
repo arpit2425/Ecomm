f6775eb3a62fb37a4e8756939e1d3b412efd6a2d
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-29 11:15:59 +0530

    logo and favicons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./../assets/logo.svg";
+import logo from "./../assets/ecomm.svg";
 import { Link } from "react-router-dom";
 export default function Header() {
   return (
