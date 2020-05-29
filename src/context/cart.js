fe6f07710c355445016d9c3b0b26f07b65d6c1d3
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-29 22:58:39 +0530

    localstorage

--- src/context/cart.js
+++ src/context/cart.js
@@ -1,16 +1,16 @@
 import React from "react";
 import localCart from "../utils/localCart";
 
-// function getCartFromLocalStorage() {
-//   return localStorage.getItem("cart")
-//     ? JSON.parse(localStorage.getItem("cart"))
-//     : [];
-// }
+function getCartFromLocalStorage() {
+  return localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : [];
+}
 
 const CartContext = React.createContext();
 
 function CartProvider({ children }) {
-  const [cart, setCart] = React.useState([]);
+  const [cart, setCart] = React.useState(getCartFromLocalStorage());
   const [total, setTotal] = React.useState(0);
   const [cartItems, setCartItems] = React.useState(0);
 
